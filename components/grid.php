64af6ce16f1b0d36b60f2758c2f11622c91cfa0c
andrew-macpherson <andrewmac17@gmail.com>
2017-07-13 13:50:10 -0400

    added background image / color / padding / margin to cotainer

--- components/grid.php
+++ components/grid.php
@@ -4,15 +4,25 @@ function container($atts,$content){
 	extract( shortcode_atts( array(
         'class' => '',
         'inner_class' => '',
-        'fluid' => 'false'
+        'fluid' => 'false',
+        'background_color' => '',
+        'background_image' => '',
+        'padding' => '',
+        'margin' => ''
     ), $atts ) );
 
 
     $content = do_shortcode( shortcode_unautop( $content ) );
     $content = stripParagraphs($content);
 
+     if($background_color != '' || $background_image != ''  || $padding != ''  || $margin != ''){
+        $openStyle = true;
+    }else{
+        $openStyle = false;
+    }
+
     $return = '';
-    $return .= '<div class="container'.(($fluid == 'true')? '-fluid ':'').' '.(( $class != '')? $class : "" ).'"><div class="'.$inner_class.'">'.force_balance_tags($content).'</div></div>';
+    $return .= '<div class="container'.(($fluid == 'true')? '-fluid ':'').' '.(( $class != '')? $class : "" ).'" '.(($openStyle) ? 'style="':'').' '.(($background_image != '') ? 'background-image: url('.$background_image.'); ':'').' '.(($background_color != '') ? 'background-color: '.$background_color.'; ':'').' '.(($padding != '') ? 'padding: '.$padding.'; ':'').' '.(($margin != '') ? 'margin: '.$margin.'; ':'').'><div class="'.$inner_class.'">'.force_balance_tags($content).'</div></div>';
 
 	return $return;
 }
