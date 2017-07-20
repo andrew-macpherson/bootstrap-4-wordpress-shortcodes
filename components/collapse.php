e3afe9987ca93e26be7b3b17a1ba72389dd4bb1f
Mitchell Tracy <mitchell@netgainseo.com>
2017-07-20 16:40:47 -0400

    Accordion Single Item

--- components/collapse.php
+++ components/collapse.php
@@ -1,21 +1,30 @@
-<?php 
+<?php
 
 function collapse($atts,$content){
 	extract( shortcode_atts( array(
-        'class' 		=> '',
-        'id'            => ''
-    ), $atts ) );
+        'class' 		=> 'btn btn-primary',
+        'id'        => '',
+				'href'			=> '',
+				'expanded'  => 'false',
+				'content'   => $content,
+				'linktext' 	=> ''
+     ), $atts ) );
 
 
     $content = do_shortcode( shortcode_unautop( $content ) );
     $content = stripParagraphs($content);
 
     $return = '';
+		$return .= '<p>
+		  <a class="'.$class.'" data-toggle="collapse" href="#'.$href.'" aria-expanded="'.$expanded.'" aria-controls="'.$href.'">';
+		 $return .= $linktext;
+		$return .= '</a>
+		</p>
+		<div class="collapse" id="'.$href.'">
+		  <div class="card card-block">';
+		  $return .=  $content;
+		$return .= '</div></div>';
 
-	$return .= '<div class="collapse" id="'.$id.'">'.$content.'</div>';
-
-	return $return;
+		return $return;
 }
 add_shortcode('collapse','collapse');
-
-
