f6286fc30d83eb8df6ac2cf12e9f3013e30aadb5
Mitchell Tracy <mitchell@netgainseo.com>
2017-07-20 12:14:32 -0400

    Alerts Close True/False

--- components/alert.php
+++ components/alert.php
@@ -1,10 +1,11 @@
-<?php 
+<?php
 //Alert
 function alert($atts,$content){
 	extract( shortcode_atts( array(
         'class' => '',
         'title' => '',
-        'type'  => 'success'
+        'type'  => 'success',
+				'close' => 'false'
     ), $atts ) );
 
 
@@ -13,8 +14,13 @@ function alert($atts,$content){
 
     $return = '';
 
-    $return .= '<div class="alert alert-'.$type.'" role="alert">
-				  '.(($title != '')? "<strong>".$title."</strong>":"").' '.force_balance_tags($content).'
+    $return .= '<div class="alert alert-'.$type.'" role="alert">';
+		if($close == 'true'){
+				$return .= '<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+									    <span aria-hidden="true">&times;</span>
+									  </button>';
+		}
+			$return .= ''.(($title != '')? "<strong>".$title."</strong>":"").' '.force_balance_tags($content).'
 				</div>';
 
 	return $return;
